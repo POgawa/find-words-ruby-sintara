e3a91334d70644a463bff0fd778937489c20de79
Paul Ogawa <paul.ogawa7@gmail.com>
2015-04-24 11:11:30 -0700

    adding second test and passing

--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -7,4 +7,9 @@ describe("String#words") do
   it(' return how frequently a word appears in a given string') do
     expect(("word, word, word, things").words("things")).to(eq(1))
   end
+
+  it('returns 3 if there are three words in the sentence') do
+    expect(("words, words, words, things").words("words")).to(eq(3))
+  end
+
 end
