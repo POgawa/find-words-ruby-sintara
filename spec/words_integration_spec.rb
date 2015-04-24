c47874db844fa81babf7c410419a69684da80940
Paul Ogawa <paul.ogawa7@gmail.com>
2015-04-24 12:03:42 -0700

    it adds functional integration tests and passing all tests

--- spec/words_integration_spec.rb
+++ spec/words_integration_spec.rb
@@ -2,3 +2,15 @@ require "words"
 require "capybara/rspec"
 require "./app"
 Capybara.app = Sinatra::Application
+
+
+
+describe('words path', {:type => :feature}) do
+  it('allows users to input a sentence and to see if a word is included in that sentence and how many times') do
+    visit('/')
+    fill_in('word', :with => 'things, things, things, words')
+    fill_in('input_word', :with => 'words')
+    click_button('Send')
+    expect(page).to have_content('1')
+  end
+end
